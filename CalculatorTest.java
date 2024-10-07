a73458bfca4e04f258ad2955a2f1ab1c5e31c1e1
MadhurNehete <nehetemadhur@gmail.com>
2024-10-07 23:29:43 +0530

    dev added new file

---
+++ CalculatorTest.java
@@ -0,0 +1,15 @@
+package student;
+
+public class CalculatorTest {
+
+	public static void main(String[] args) {
+	    Calculator a1=new Calculator();
+	    System.out.println("This is the mini project:");
+	    a1.add(10,20);
+	    a1.sub(10,20);
+	    a1.mul(10,20);
+	    a1.div(10,20);
+
+	}
+
+}
