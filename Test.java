8b956e3bb12d7ce2d28e4534ff4508463052b592
MadhurNehete <132289905+MadhurNehete@users.noreply.github.com>
2024-10-07 23:56:11 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,19 +3,30 @@ package sit1;
 public class Test {
 
 	public static void main(String[] args) {
-		A a=new A();	
-		a.m1(112,22);
+		A a=new A();
+		
+		   a.m1(112,22);
+		
 		System.out.println(a.b);
-		a.m2(62,84);
+		
+		   a.m2(62,84);
+		
 		System.out.println(a.a);
 		
-		B b1=new B();
+		   B b1=new B();
+		
 		System.out.println(b1.c);
-		b1.m2(222,111);
+		
+		   b1.m2(222,111);
+		
 		System.out.println(b1.d);
-		b1.m3();
+		
+		   b1.m3();
+		
 		System.out.println(b1.d);
+		
 		System.out.println(a.b);
+		
 		
 
 	}
